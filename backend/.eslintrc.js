28ece98722ca871980f155351162720da1470513
Igor Tkach <itkach@gmail.com>
2023-09-08 10:17:36 -0400

    bump eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   rules: {
     "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
