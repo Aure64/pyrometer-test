fd4b6e5000973cf11f06e7af1b8a57e6c092f171
Igor Tkach <itkach@gmail.com>
2023-09-08 10:09:08 -0400

    add protocol o tests

---
+++ backend/src/testFixtures/o/block93416rights.ts
@@ -0,0 +1,12 @@
+import { BakingRightsO } from "../../rpc/types";
+
+const rights: BakingRightsO = [
+  {
+    level: 93416,
+    delegate: "tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9",
+    round: 0,
+    consensus_key: "tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9",
+  },
+];
+
+export default rights;
