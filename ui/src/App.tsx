8a1eedbf3a4f8bc468db119e6dfb9d6bb622de34
Aurelien <aurelien@example.com>
2026-02-24 19:27:41 +0100

    feat: add Settings tab to main app navigation

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,20 @@
-import { VStack, HStack, IconButton, useColorMode } from '@chakra-ui/react';
+import {
+  VStack,
+  HStack,
+  IconButton,
+  useColorMode,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from '@chakra-ui/react';
 import React from 'react';
-import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import { MdDarkMode, MdLightMode, MdSettings } from 'react-icons/md';
 import SystemInfo from './SystemInfo';
 import Bakers from './Bakers';
 import Nodes from './Nodes';
+import Settings from './Settings';
 import { useSettingsQuery } from './api';
 
 interface AppProps {}
@@ -22,18 +33,38 @@ function App({}: AppProps) {
 
   return (
     <VStack p="20px" alignItems="flex-start" w="100%">
-      <HStack w="100%" justifyContent="flex-end">
-        <IconButton
-          aria-label="Toggle dark mode"
-          icon={colorMode === 'dark' ? <MdLightMode /> : <MdDarkMode />}
-          onClick={toggleColorMode}
-          variant="ghost"
-          size="sm"
-        />
-      </HStack>
-      <Bakers isVisible={isVisible} />
-      <Nodes isVisible={isVisible} />
-      {data?.settings.showSystemInfo && <SystemInfo />}
+      <Tabs w="100%" variant="enclosed" isLazy>
+        <HStack w="100%" justifyContent="space-between">
+          <TabList>
+            <Tab>Bakers</Tab>
+            <Tab>Nodes</Tab>
+            <Tab>
+              <MdSettings style={{ marginRight: 4 }} /> Settings
+            </Tab>
+          </TabList>
+          <IconButton
+            aria-label="Toggle dark mode"
+            icon={
+              colorMode === 'dark' ? <MdLightMode /> : <MdDarkMode />
+            }
+            onClick={toggleColorMode}
+            variant="ghost"
+            size="sm"
+          />
+        </HStack>
+        <TabPanels>
+          <TabPanel px={0}>
+            <Bakers isVisible={isVisible} />
+            {data?.settings.showSystemInfo && <SystemInfo />}
+          </TabPanel>
+          <TabPanel px={0}>
+            <Nodes isVisible={isVisible} />
+          </TabPanel>
+          <TabPanel px={0}>
+            <Settings />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </VStack>
   );
 }
